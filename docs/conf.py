86f963d1f2c216da63a2b67701f83957ec9c5555
Deepansh Srivastava <deepansh2012@gmail.com>
2019-08-16 15:49:25 -0400

    update docs and version

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ author = "Deepansh J. Srivastava"
 # The short X.Y version
 version = "0.1"
 # The full version, including alpha/beta/rc tags
-release = "0.1.0a0"
+release = "0.1-beta.1"
 
 
 # -- General configuration ---------------------------------------------------
