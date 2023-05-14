7ecfc01920f5dae135783852dbbf5d0bc5e84395
deepanshs <21365911+deepanshs@users.noreply.github.com>
2023-05-14 16:01:42 -0400

    version update

--- csdmpy/__init__.py
+++ csdmpy/__init__.py
@@ -32,7 +32,7 @@ __credits__ = ["Deepansh J. Srivastava"]
 __license__ = "BSD License"
 __maintainer__ = "Deepansh J. Srivastava"
 __status__ = "Beta"
-__version__ = "rc0.6.0"
+__version__ = "0.6.dev2"
 
 __all__ = [
     "parse_dict",
